40f9c0b5725822b22446e6cc928082bc2ace22d4
Denis Verevin <verevin@mercurydevelopment.com>
2021-08-04 17:59:53 +0400

    Clean up

--- src/hooks/api/useSignUp.tsx
+++ src/hooks/api/useSignUp.tsx
@@ -1,11 +1,14 @@
 import { AxiosError } from "axios";
 import { useRouter } from "next/router";
 import { useMutation } from "react-query";
+
 import { useRequest } from "src/hooks/useRequest";
 import { SignUpRequest } from "src/types/auth/signup";
 import { ApiError } from "src/types/error";
+
 import { ROUTE_DEFAULT } from "src/utils/route";
-import { useAccessToken } from "../useAccessToken";
+
+import { useAccessToken } from "src/hooks/useAccessToken";
 
 type AuthResponse = { accessToken: string };
 
