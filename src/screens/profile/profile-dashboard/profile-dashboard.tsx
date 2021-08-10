322f68f766a7a29bceab2e66c1a956928ab796eb
Denis Verevin <verevin@mercurydevelopment.com>
2021-08-11 01:25:25 +0700

    Add hook useMeRaceView

--- src/screens/profile/profile-dashboard/profile-dashboard.tsx
+++ src/screens/profile/profile-dashboard/profile-dashboard.tsx
@@ -1,10 +1,15 @@
+import Section from "src/components/section";
 import Title from "src/components/title"
+import { useMeRaceView } from "src/hooks/api/useMeRaceView";
 import MESSAGES from './profile-dashboard.messages';
 
 const ProfileDashboard = () => {
+  const {data: raceMe} = useMeRaceView();
   return(
     <>
     <Title>{MESSAGES.pageTitle}</Title>
+    <Section>
+    </Section>
     </>
   )
 }
